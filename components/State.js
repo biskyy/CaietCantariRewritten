2b522867541f4a15e4c0830a6778a4a1e70109bc
biskyy <54770799+biskyy@users.noreply.github.com>
2024-02-22 12:54:23 +0200

    Added useTheme hook
    Also moved the cacheFonts function from App to here.

--- components/State.js
+++ components/State.js
@@ -1,9 +1,48 @@
-import { atom } from "jotai";
-import { atomWithStorage, createJSONStorage } from "jotai/utils";
+import { atom, useAtom } from "jotai";
+import { atomWithStorage, createJSONStorage, loadable } from "jotai/utils";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Cantari from "../assets/Cantari.json";
+import NetInfo from "@react-native-community/netinfo";
+import { Alert } from "react-native";
+import * as Font from "expo-font";
 
 const storage = createJSONStorage(() => AsyncStorage);
-export const themeAtom = atomWithStorage("theme", 0, storage);
+export const themeAtom = atomWithStorage("theme", "not set", storage, {
+  getOnInit: true,
+});
 export const songsAtom = atomWithStorage("songs", Cantari, storage);
 export const isLoadingAtom = atom(0);
+export const userAtom = atomWithStorage(
+  "user",
+  { loggedIn: false, token: "" },
+  storage
+);
+
+export const checkInternetConnection = () => {
+  NetInfo.fetch().then((internet) => {
+    if (!internet.isConnected)
+      Alert.alert(
+        "Nu exista conexiune la internet",
+        "Conecteaza-te la internet"
+      );
+  });
+};
+
+export const checkConnectionToServer = () => {}; // to be added
+
+export const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font)); // cache fonts method
+
+const readOnlyLoadableThemeAtom = loadable(themeAtom);
+
+const writeableLoadableThemeAtom = atom(
+  (get) => get(readOnlyLoadableThemeAtom),
+  async (_get, set, arg) => set(themeAtom, arg)
+);
+
+/**
+ * @returns {Object}
+ */
+export const useTheme = () => {
+  const [theme, setTheme] = useAtom(writeableLoadableThemeAtom);
+  return [theme, setTheme];
+};
