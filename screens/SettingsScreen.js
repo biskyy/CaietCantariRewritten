bd73ef7360c0e0813c63f47d7b38631623279dbd
biskyy <54770799+biskyy@users.noreply.github.com>
2024-02-22 12:57:52 +0200

    Update SettingsScreen.js

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,25 +1,37 @@
 import { Text, StyleSheet, View } from "react-native";
 import ThemeColors from "../components/ColorScheme";
-import { themeAtom } from "../components/State";
+import { useTheme } from "../components/State";
 import { useAtom } from "jotai";
+import Button from "../components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export default function SettingsScreen() {
-  const [theme] = useAtom(themeAtom);
+  const [theme] = useTheme();
+
+  const navigation = useNavigation();
 
   const themeStyle = StyleSheet.create({
     bgColor: {
-      backgroundColor: theme
+      backgroundColor: theme.data
         ? ThemeColors.darkBgColor
         : ThemeColors.lightBgColor,
     },
     txtColor: {
-      color: theme ? ThemeColors.darkTxtColor : ThemeColors.lightTxtColor,
+      color: theme.data ? ThemeColors.darkTxtColor : ThemeColors.lightTxtColor,
     },
   });
 
   return (
     <>
-      <View style={[themeStyle.bgColor, styles.settingsDiv]}></View>
+      <View style={[themeStyle.bgColor, styles.settingsDiv]}>
+        <Button
+          textStyle={[themeStyle.txtColor, styles.loginButtonText]}
+          touchableStyle={[themeStyle.bgColor, styles.loginButton]}
+          // @ts-ignore
+          onPress={() => navigation.navigate("Login")}
+          text="Login"
+        />
+      </View>
     </>
   );
 }
@@ -27,5 +39,16 @@ export default function SettingsScreen() {
 const styles = StyleSheet.create({
   settingsDiv: {
     height: "100%",
+    alignItems: "center",
+    paddingTop: 20,
+  },
+  loginButton: {
+    width: "100%",
+    alignItems: "center",
+    padding: 20,
+  },
+  loginButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
