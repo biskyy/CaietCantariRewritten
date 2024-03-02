02a7be2816bec1f9c3db09e2c846988df00d895b
biskyy <54770799+biskyy@users.noreply.github.com>
2024-03-02 18:53:02 +0200

    Added placeholder colors

--- components/ColorScheme.js
+++ components/ColorScheme.js
@@ -1,7 +1,9 @@
 const ThemeColors = {
   darkBgColor: "#0a0d0c",
   darkTxtColor: "#f1f4f3",
+  darkPlaceholderTxtColor: "#8e8e8e",
   lightBgColor: "#f0f4fa",
   lightTxtColor: "#04060a",
+  lightPlaceholderTxtColor: "#636c72",
 };
 export default ThemeColors;
