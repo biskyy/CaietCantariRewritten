73e355bc3ac4cb978ff6f12b59ad68789a02cb5b
biskyy <54770799+biskyy@users.noreply.github.com>
2024-03-02 22:27:09 +0200

    Fixed theming on android

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import "react-native-gesture-handler";
 import { Suspense, useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import {
-  CardStyleInterpolators,
-  createStackNavigator,
-} from "@react-navigation/stack";
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import * as SplashScreen from "expo-splash-screen";
 
 // Screens
@@ -17,8 +18,10 @@ import Navbar from "./components/Navbar";
 import { MaterialIcons } from "@expo/vector-icons";
 import LoginScreen from "./screens/LoginScreen";
 import EditSongScreen from "./screens/EditSong";
-import { useColorScheme } from "react-native";
+import { View, useColorScheme } from "react-native";
 import { cacheFontsAndIcons, useTheme } from "./components/State";
+import * as SystemUI from "expo-system-ui";
+
 const Stack = createStackNavigator();
 
 SplashScreen.preventAutoHideAsync();
@@ -38,6 +41,11 @@ export default function App() {
   }, []);
 
   useEffect(() => {
+    if (theme.data) SystemUI.setBackgroundColorAsync("#0a0d0c");
+    else SystemUI.setBackgroundColorAsync("#f0f4fa");
+  }, [theme.data]);
+
+  useEffect(() => {
     if (theme.state !== "loading") {
       if (theme.data === "not set")
         if (colorScheme === "light") setTheme(false);
@@ -48,31 +56,34 @@ export default function App() {
 
   return (
     // <Suspense fallback={<View style={{ backgroundColor: "blue" }}></View>}>
-    //   <Suspense fallback={<View style={{ backgroundColor: "red" }}></View>}>
-    <>
-      <LoadingScreen />
+    // <View style={{ flex: 1, ...themeStyle.bgColor }}>
+    <Suspense
+      fallback={<View style={{ flex: 1, backgroundColor: "blue" }}></View>}
+    >
+      <>
+        <LoadingScreen />
 
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={{
-            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS, // new screen slides from right to left on android
-            header: () => <Navbar />,
-          }}
-        >
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ header: () => <></> }}
-          />
-          <Stack.Screen name="Song" component={SongScreen} />
-          <Stack.Screen name="Settings" component={SettingsScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="EditSong" component={EditSongScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
-    //   </Suspense>
+        <NavigationContainer theme={theme.data ? DarkTheme : DefaultTheme}>
+          <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              header: () => <Navbar />,
+            }}
+          >
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{ header: () => <></> }}
+            />
+            <Stack.Screen name="Song" component={SongScreen} />
+            <Stack.Screen name="Settings" component={SettingsScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="EditSong" component={EditSongScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </>
+    </Suspense>
+    // </View>
     // </Suspense>
   );
 }
