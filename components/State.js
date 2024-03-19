fb7f7e2acdd1e255455e15720721007bd5c3c3fd
biskyy <54770799+biskyy@users.noreply.github.com>
2024-03-19 15:52:35 +0200

    Moved network requests and hooks to their respective files

--- components/State.js
+++ components/State.js
@@ -1,15 +1,9 @@
-import { atom, useAtom } from "jotai";
+import { atom } from "jotai";
 import { atomWithStorage, createJSONStorage, loadable } from "jotai/utils";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Cantari from "../assets/Cantari.json";
-import NetInfo from "@react-native-community/netinfo";
-import { Alert } from "react-native";
-import * as Font from "expo-font";
-import axios from "axios";
-import { StyleSheet } from "react-native";
-import ThemeColors from "./ColorScheme";
 
-export const serverApi = "https://caiet-de-cantari.biskyys-api.net";
+export const apiUrl = "https://caiet-de-cantari.biskyys-api.net";
 
 const storage = createJSONStorage(() => AsyncStorage);
 export const songsAtom = atomWithStorage("songs", Cantari, storage);
@@ -27,30 +21,6 @@ export const loadingScreenAtom = atom({
 });
 
 /**
- * @typedef {Object} LoadingScreenObject
- * @property {number} state
- * @property {string} message
- * @property {(...args: any[]) => void} callback
- *
- * @returns {[LoadingScreenObject, Function: (object: Partial<LoadingScreenObject>) => void]}
- */
-
-export const useLoadingScreen = () => {
-  const [loadingScreen, _setLoadingScreen] = useAtom(loadingScreenAtom);
-  const setLoadingScreen = (newKey) => {
-    _setLoadingScreen((prevObject) => ({ ...prevObject, ...newKey }));
-  };
-  return [loadingScreen, setLoadingScreen];
-};
-
-const dispalyedSongInfoAtom = atom({
-  song: {},
-  index: 0,
-  listFirstIndex: 0,
-  listLastIndex: 0,
-});
-
-/**
  * @typedef {Object} SongObject
  * @property {string} title
  * @property {string} content
@@ -60,31 +30,12 @@ const dispalyedSongInfoAtom = atom({
  * @property {Array<string>} tags
  */
 
-/**
- *
- * @typedef {Object} DisplayedSongInfo
- * @property {Partial<SongObject>} song
- * @property {number} index
- * @property {number} listFirstIndex
- * @property {number} listLastIndex
- *
- * @returns {[DisplayedSongInfo, Function: (object: Partial<DisplayedSongInfo>) => void]}
- */
-
-export const useDisplayedSongInfo = () => {
-  const [displayedSongInfo, _setDispalyedSongInfo] = useAtom(
-    dispalyedSongInfoAtom
-  );
-
-  const setDispalyedSongInfo = (newKeys) => {
-    _setDispalyedSongInfo((prevObject) => ({
-      ...prevObject,
-      ...newKeys,
-    }));
-  };
-
-  return [displayedSongInfo, setDispalyedSongInfo];
-};
+export const dispalyedSongInfoAtom = atom({
+  song: {},
+  index: 0,
+  listFirstIndex: 0,
+  listLastIndex: 0,
+});
 
 export const themeAtom = atomWithStorage("theme", "not set", storage, {
   getOnInit: true,
@@ -92,147 +43,7 @@ export const themeAtom = atomWithStorage("theme", "not set", storage, {
 
 const readOnlyLoadableThemeAtom = loadable(themeAtom);
 
-const writeableLoadableThemeAtom = atom(
+export const writeableLoadableThemeAtom = atom(
   (get) => get(readOnlyLoadableThemeAtom),
   async (_get, set, arg) => set(themeAtom, arg)
 );
-
-/**
- * @returns {[{data?: boolean | string, state: string}, (arg: boolean) => void]}
- */
-export const useTheme = () => {
-  const [theme, setTheme] = useAtom(writeableLoadableThemeAtom);
-  return [theme, setTheme];
-};
-
-export const useThemeStyle = () => {
-  const [theme] = useTheme();
-  const themeStyle = StyleSheet.create({
-    bgColor: {
-      backgroundColor: theme.data
-        ? ThemeColors.darkBgColor
-        : ThemeColors.lightBgColor,
-    },
-    txtColor: {
-      color: theme.data ? ThemeColors.darkTxtColor : ThemeColors.lightTxtColor,
-    },
-    inverseBgColor: {
-      backgroundColor: theme.data
-        ? ThemeColors.lightBgColor
-        : ThemeColors.darkBgColor,
-    },
-    separatorColor: {
-      backgroundColor: theme.data
-        ? ThemeColors.lightPlaceholderTxtColor
-        : ThemeColors.darkPlaceholderTxtColor,
-    },
-    inverseTxtColor: {
-      color: theme.data ? ThemeColors.lightTxtColor : ThemeColors.darkTxtColor,
-    },
-    borderColor: {
-      borderColor: theme.data
-        ? ThemeColors.lightPlaceholderTxtColor
-        : ThemeColors.darkPlaceholderTxtColor,
-    },
-  });
-
-  return themeStyle;
-};
-
-/**
- * @returns boolean
- */
-export const isInternetConnected = async () => {
-  const internet = await NetInfo.fetch();
-  if (!internet.isConnected)
-    Alert.alert(
-      "Nu exista conexiune la internet",
-      "Este necesara o conexiune la internet pentru a realiza o cerere."
-    );
-  return internet.isConnected;
-};
-
-/**
- *
- * @param {Object} config
- * @returns {Promise<{data: any, status: number}>}
- */
-export const fetchSongsRequest = async (config) => {
-  if (!(await isInternetConnected())) return { data: undefined, status: 400 };
-
-  try {
-    const response = await axios.get(`${serverApi}/songs`, {
-      timeout: 5000,
-      ...config,
-    });
-    return { data: response.data, status: response.status };
-  } catch (error) {
-    return handleErrorResponse(error);
-  }
-};
-
-/**
- *
- * @param {string} username
- * @param {string} password
- * @returns {Promise<{data: Object, status: number}>}
- */
-export const loginRequest = async (username, password) => {
-  if (!(await isInternetConnected())) return { data: undefined, status: 400 };
-
-  try {
-    const response = await axios.post(
-      `${serverApi}/login`,
-      { username, password },
-      { timeout: 5000 }
-    );
-    return { data: response.data, status: response.status };
-  } catch (error) {
-    return handleErrorResponse(error);
-  }
-};
-
-/**
- * @param {SongObject} updatedSong
- */
-
-export const updateSongRequest = async (updatedSong, token) => {
-  if (!(await isInternetConnected())) return { data: undefined, status: 400 };
-
-  try {
-    const response = await axios.put(`${serverApi}/song`, updatedSong, {
-      headers: {
-        authorization: token,
-      },
-      timeout: 5000,
-    });
-    return { data: response.data, status: response.status };
-  } catch (error) {
-    return handleErrorResponse(error);
-  }
-};
-
-const handleErrorResponse = (error) => {
-  if (error.response) {
-    Alert.alert(
-      `Eroare: ${error.response.status}`,
-      `${error.response.data.message}`
-    );
-    return { data: undefined, status: error.response.status };
-  } else if (error.request) {
-    Alert.alert(
-      "Serverul este offline",
-      "Nu s-a putut efectua cererea deoarece serverul nu este online."
-    );
-    return { data: undefined, status: 500 };
-  } else {
-    Alert.alert(
-      "Cererea este invalida",
-      `${error.message}\n\nTrimite un screenshot la developer ;)`
-    );
-    return { data: undefined, status: 400 };
-  }
-};
-
-export const cacheFontsAndIcons = (fonts) =>
-  fonts.map((font) => Font.loadAsync(font)); // cache fonts method
