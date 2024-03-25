0e9d69955e723f2b51b98699ceee997d2ff4e738
biskyy <54770799+biskyy@users.noreply.github.com>
2024-03-25 21:32:25 +0200

    Delete ColorScheme.js

--- components/ColorScheme.js
+++
@@ -1,9 +0,0 @@
-const ThemeColors = {
-  darkBgColor: "#0a0d0c",
-  darkTxtColor: "#f1f4f3",
-  darkPlaceholderTxtColor: "#71717a",
-  lightBgColor: "#f0f4fa",
-  lightTxtColor: "#04060a",
-  lightPlaceholderTxtColor: "#6b7280",
-};
-export default ThemeColors;
