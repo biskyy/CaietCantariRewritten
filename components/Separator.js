91a675f2d4532a24ab39c4d3ad8908c2a5d7bf37
biskyy <54770799+biskyy@users.noreply.github.com>
2024-09-01 22:28:59 +0300

    Allow width to be customizable

--- components/Separator.js
+++ components/Separator.js
@@ -2,15 +2,17 @@ import { memo } from "react";
 import { StyleSheet, View } from "react-native";
 import { useThemeStyle } from "./Hooks";
 
-function Separator() {
+function Separator(props) {
   const themeStyle = useThemeStyle();
 
-  return <View style={[styles.separator, themeStyle.separatorColor]} />;
+  return (
+    <View style={[styles.separator, themeStyle.separatorColor, props.style]} />
+  );
 }
 
 const styles = StyleSheet.create({
   separator: {
-    width: "100%",
+    // width: "100%",
     height: 1,
   },
 });
