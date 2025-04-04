85161402e48f5a398c1786598d4e55b735c7120e
biskyy <andreisirbu07@gmail.com>
2025-04-04 11:56:27 +0300

    test

---
+++ app/_layout.tsx
@@ -0,0 +1,56 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+import * as ScreenOrientation from "expo-screen-orientation";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+    const unlockScreenOerientation = async () => {
+      console.log("hi");
+      await ScreenOrientation.unlockAsync();
+    };
+    unlockScreenOerientation();
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerTransparent: true,
+          headerBlurEffect: "regular",
+          orientation: "all",
+        }}
+      >
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
