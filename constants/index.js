c481586847cff87f1fcd4621de93215ff9eee7e4
biskyy <andreisirbu07@gmail.com>
2025-04-28 23:28:53 +0300

    refactor: define and use constants for accessing the storage

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+export const API_URL =
+  "https://oi2ahiamhhmtrhwgsi2z43wob40wfgbe.lambda-url.eu-central-1.on.aws";
+//export const API_URL = "https://caiet-de-cantari.biskyys-api.net";
+// export const API_URL = "http://192.168.1.61:3000/caiet-de-cantari";
+
+export const STORAGE_SONGS = "storage/songs";
+
+export const STORAGE_SONG_FONT_SIZE = "storage/song-font-size";
+
+export const STORAGE_USER = "storage/user";
+export const STORAGE_USER_PREFS = "storage/user/prefs";
+export const STORAGE_USER_FAVORITE_SONGS = "storage/user/favorite-songs";
+
+export const STORAGE_THEME = "storage/theme";
